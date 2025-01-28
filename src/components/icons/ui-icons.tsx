eba2cf098099d714f1ef9a880e25fc8b6b851e59
ayat-delineate <ayat@delineate.pro>
2025-01-29 01:26:16 +0600

    add className prop to IconWhiteCheck for improved styling flexibility

--- src/components/icons/ui-icons.tsx
+++ src/components/icons/ui-icons.tsx
@@ -56,6 +56,7 @@ export const IconWhiteCheck: React.FC<IconProps> = ({ className }) => {
     <svg
       width="14"
       height="11"
+      className={className}
       viewBox="0 0 14 11"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
