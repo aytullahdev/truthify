11b24484ba1c7482aa2e547279e730ee8b3683ed
ayat-delineate <ayat@delineate.pro>
2025-02-01 02:15:04 +0600

    fix: remove unnecessary class from SidebarProvider and adjust SidebarTrigger visibility for improved layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,12 +3,11 @@ import { AppSidebar } from "./app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider className="bg-white">
+    <SidebarProvider>
       <AppSidebar />
-      <SidebarTrigger />
-
       <SidebarInset className="relative">
         <header className="flex  shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border h-14 sticky top-0 z-10 bg-white ">
+          <SidebarTrigger className="block lg:hidden" />
           <p className="px-5 text-[#292929] font-semibold">Dashboard</p>
         </header>
         <div className="flex flex-1 flex-col  min-w-full">{children}</div>
