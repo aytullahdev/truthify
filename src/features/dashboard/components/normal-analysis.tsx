7a530fac4f89fdb38c37022e62be5b5dd1e76a47
ayat-delineate <ayat@delineate.pro>
2025-02-03 18:20:21 +0600

    feat: pass additional props to ComparativeAnalysis component for enhanced data display

--- src/features/dashboard/components/normal-analysis.tsx
+++ src/features/dashboard/components/normal-analysis.tsx
@@ -1,11 +1,15 @@
 import { ComparativeAnalysis } from "./comparative-analysis";
 import { IntegrityToPrice } from "./integrity-to-price";
-
+const NORMAL_ANALYSIS_DATA = {
+  position: 6,
+  imgSrc: "satynadella.png",
+  name: "Satya Nadella",
+};
 export const NormalAnalysis = () => {
   return (
     <div className="flex flex-col gap-5 overflow-y-auto">
       <IntegrityToPrice />
-      <ComparativeAnalysis />
+      <ComparativeAnalysis {...NORMAL_ANALYSIS_DATA} />
     </div>
   );
 };
